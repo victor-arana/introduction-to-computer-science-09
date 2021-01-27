084d3c8959121064efd71d9cfa38b5402ae909d6
no.data <victor_arana@hotmail.com>
2021-01-27 16:15:14 -0600

    add print statements
    Changes committed:
    	modified:   01-square-root.py

--- lectures/01-introduction-to-python/01-square-root.py
+++ lectures/01-introduction-to-python/01-square-root.py
@@ -1,6 +1,9 @@
-g = 4
-x = 25
+g = 3 
+x = 16 
 epsilon = 0.1
-while abs( x - g**2 ) <= epsilon:
+print("x:", x, "g:", g, "g^2:", g**2, "diff:", abs(x - g**2))
+while abs( x - g**2 ) > epsilon:
+    print("x:", x, "g:", g, "g^2:", g**2, "x/g:", x/g, "(g + x/g)/2", (g + x/g)/2,"diff:", abs(x - g**2))
     g = (g + x/g)/2
-print('guess:', g)
+print("x:", x, "g:", g, "g^2:", g**2, "x/g:", x/g, "(g + x/g)/2", (g + x/g)/2,"diff:", abs(x - g**2))
+    
