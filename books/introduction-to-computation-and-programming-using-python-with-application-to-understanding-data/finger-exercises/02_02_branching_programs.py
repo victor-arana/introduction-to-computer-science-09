4a15260aebca10a461d07c63c41f834315589709
victor-arana <victor_arana@hotmail.com>
2021-02-06 10:50:54 -0600

    pass unit tests

--- books/introduction-to-computation-and-programming-using-python-with-application-to-understanding-data/finger-exercises/02_02_branching_programs.py
+++ books/introduction-to-computation-and-programming-using-python-with-application-to-understanding-data/finger-exercises/02_02_branching_programs.py
@@ -2,6 +2,20 @@
 # odd number among them. If none of them are odd, it should
 # print a message to that effect
 
+def filter_odd_numbers(numbers):
+    odd_numbers = []
+    for n in numbers:
+        if n % 2 != 0:
+            odd_numbers.append(n)
+    return odd_numbers
+
+def max_number(numbers):
+    max_num = numbers[0]
+    for n in numbers:
+        if n >= max_num:
+            max_num = n
+    return max_num
+
 def largest_odd_number(x, y, z):
     '''
     x: integer number
@@ -11,17 +25,21 @@ def largest_odd_number(x, y, z):
             odd print a message to that effect
     '''
     largest_odd_number = None
-    if z%2!=0:
-        largest_odd_number = z
-    return largest_odd_number
+    numbers = [x, y, z]
+    odd_numbers = filter_odd_numbers(numbers)
+    if len(odd_numbers) > 0:
+        return max_number(odd_numbers)
+    else: 
+        return None
+    
 
 def largest_odd_number_test():
     # 0: expected value
     # 1-3: arguments
     test_cases = []
-    test_cases.append([3,1,3,5]) 
-    test_cases.append([3,5,1,3]) 
-    test_cases.append([3,3,5,1])
+    test_cases.append([5,1,3,5]) 
+    test_cases.append([5,5,1,3]) 
+    test_cases.append([5,3,5,1])
     for t in test_cases:
         actual = largest_odd_number(t[1],t[2],t[3])
         expected = t[0]
