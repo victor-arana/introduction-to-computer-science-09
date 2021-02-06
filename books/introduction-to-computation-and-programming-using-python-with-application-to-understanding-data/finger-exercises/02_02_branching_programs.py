dc34eb0e2dfe7b50107dea8a89c9d755322c957c
victor-arana <victor_arana@hotmail.com>
2021-02-06 10:34:01 -0600

    :test-cases: write test cases
    Changes committed:
    	modified:   02_02_branching_programs.py

--- books/introduction-to-computation-and-programming-using-python-with-application-to-understanding-data/finger-exercises/02_02_branching_programs.py
+++ books/introduction-to-computation-and-programming-using-python-with-application-to-understanding-data/finger-exercises/02_02_branching_programs.py
@@ -10,15 +10,24 @@ def largest_odd_number(x, y, z):
     return: largest odd number among x, y, and z. If none are 
             odd print a message to that effect
     '''
-    return x
+    largest_odd_number = None
+    if z%2!=0:
+        largest_odd_number = z
+    return largest_odd_number
 
 def largest_odd_number_test():
-    x = 1
-    y = 3
-    z = 5
-    if z == largest_odd_number(x,y,z):
-        print('pass')
-    else:
-        print('fail')
+    # 0: expected value
+    # 1-3: arguments
+    test_cases = []
+    test_cases.append([3,1,3,5]) 
+    test_cases.append([3,5,1,3]) 
+    test_cases.append([3,3,5,1])
+    for t in test_cases:
+        actual = largest_odd_number(t[1],t[2],t[3])
+        expected = t[0]
+        if actual == expected: 
+            print('pass', 'e:', expected, 'a:', actual, 'x:', t[1], 'y:', t[2], 'z:', t[3] )
+        else:
+            print('fail', 'e:', expected, 'a:', actual, 'x:', t[1], 'y:', t[2], 'z:', t[3] )
 
 largest_odd_number_test()
