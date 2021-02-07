abe2ee343fa5e1aecc90f3ee01d5a89dfe4bc385
victor-arana <victor_arana@hotmail.com>
2021-02-07 15:27:07 -0600

    :test-pass: pass test cases
    Changes committed:
    	modified:   02_04_iteration_02_odd_numbers.py

--- books/introduction-to-computation-and-programming-using-python-with-application-to-understanding-data/finger-exercises/chapter-02/02_04_iteration_02_odd_numbers.py
+++ books/introduction-to-computation-and-programming-using-python-with-application-to-understanding-data/finger-exercises/chapter-02/02_04_iteration_02_odd_numbers.py
@@ -5,18 +5,39 @@
 def largest_odd_number_test():
     # 0: expected value
     # 1-3: arguments
-    test_cases = []
-    test_cases.append([5,1,3,5]) 
-    test_cases.append([5,5,1,3]) 
-    test_cases.append([5,3,5,1])
-    test_cases.append(['No odd numbers', 2, 4, 6])
+    test_cases = readCSV('02_04_iteration_02_odd_numbers-test-cases.csv', ',', 'int')
+    print('result','expected','actual','arguments', sep = ',')
     for t in test_cases:
-        actual = largest_odd_number(t[1:])
         expected = t[0]
-        if actual == expected: 
-            print('pass', 'e:', expected, 'a:', actual, 'x:', t[1], 'y:', t[2], 'z:', t[3] )
-        else:
-            print('fail', 'e:', expected, 'a:', actual, 'x:', t[1], 'y:', t[2], 'z:', t[3] )
+        arguments = t[1:]
+        actual = largest_odd_number(arguments)
+        result = 'FAILURE'
+        if actual == expected:
+            result = 'success'
+        print(result, expected, actual, arguments, sep = ',')
+
+def readCSV(file_path, delim, elements_type):
+    '''
+    Reads a csv file
+    file_path: path to the file
+    delim: delimiter used in the csv file
+    elements_type: type of the elements to be returned
+    return: a list containing elements of the specified type
+    '''
+    import csv
+    rows = []
+    with open(file_path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=delim)
+        for row in csv_reader:
+            col = []
+            for column in row:
+                if elements_type == 'float':
+                    col.append(float(column))
+                elif elements_type == 'int':
+                    col.append(int(column))
+            rows.append(col)
+    return rows
+
 
 def filter_odd_numbers(numbers):
     odd_numbers = []
@@ -52,6 +73,3 @@ for i in integers_string_list:
     integers_list.append(int(i))
 
 print('largest odd number:', largest_odd_number(integers_list))
-
-
-
