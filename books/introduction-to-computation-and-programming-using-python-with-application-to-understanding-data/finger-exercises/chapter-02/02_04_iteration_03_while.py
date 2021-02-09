5e7af7b4358a9ba688de6039abd803bcc05a432c
victor-arana <victor_arana@hotmail.com>
2021-02-09 07:52:31 -0600

    finished program
    Changes committed:
    	modified:   02_04_iteration_03_while.py

--- books/introduction-to-computation-and-programming-using-python-with-application-to-understanding-data/finger-exercises/chapter-02/02_04_iteration_03_while.py
+++ books/introduction-to-computation-and-programming-using-python-with-application-to-understanding-data/finger-exercises/chapter-02/02_04_iteration_03_while.py
@@ -1,4 +1,8 @@
 numXs = int(input('How many times should I print the letter X? '))
 toPrint = ''
 #concatenate x to Print numXs times
+itersLeft = numXs
+while ( itersLeft != 0 ):
+    toPrint = toPrint + 'X'
+    itersLeft = itersLeft - 1
 print(toPrint)
