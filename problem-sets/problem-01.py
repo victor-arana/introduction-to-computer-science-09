88a31b782dca3c1736b9a6e55100e91dddf6e4ea
victor-arana <victor_arana@hotmail.com>
2021-02-10 07:36:29 -0600

    working version
    Changes to be committed:
    	modified:   problem-01.py

--- problem-sets/problem-01.py
+++ problem-sets/problem-01.py
@@ -6,9 +6,16 @@ def isVowel(char):
     '''
     return char in('a','e','i','o','u', 'A', 'E', 'I', 'O', 'U')
 
-count = 0
-for char in s:
-    if isVowel(char):
-        count = count + 1
-print('Number of vowels:', count)
+def number_of_vowels(string):
+    '''
+    string: a string
+    returns: number of vowels contained in the string
+    '''
+    count = 0
+    for char in string:
+        if isVowel(char):
+            count = count + 1
+    return count
+
+print('Number of vowels:', number_of_vowels('Hola mundo'))
 
