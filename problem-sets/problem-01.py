30b8256868c1596d6c86ce6c96307935ae208d0c
victor-arana <victor_arana@hotmail.com>
2021-02-10 07:40:54 -0600

    :refactor: reduce to one line
    Changes committed:
    	modified:   problem-01.py

--- problem-sets/problem-01.py
+++ problem-sets/problem-01.py
@@ -1,21 +1,10 @@
-def isVowel(char):
-    '''
-    char: a single letter of any case
-
-    returns: True if char is a vowel and False otherwise.
-    '''
-    return char in('a','e','i','o','u', 'A', 'E', 'I', 'O', 'U')
-
 def number_of_vowels(string):
     '''
     string: a string
     returns: number of vowels contained in the string
     '''
-    count = 0
-    for char in string:
-        if isVowel(char):
-            count = count + 1
-    return count
+    return len([2 for letter in string if letter in 'aeiou']) 
 
-print('Number of vowels:', number_of_vowels('Hola mundo'))
+s = 'Hola mundo'
+print('Number of vowels:', number_of_vowels(s))
 
